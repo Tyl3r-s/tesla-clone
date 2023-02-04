c0f9aa4dd7b2676a39846958bd614d8bcdb816d5
Tyler <Tyl3r.s@outlook.com>
2023-02-03 19:17:07 -0500

    clean up script

--- nav.js
+++ nav.js
@@ -1,37 +1,9 @@
-// document.addEventListener('click', e => {
-   
-//     e.preventDefault();
-
-//     const isDropdownButton = e.target.matches("[data-dropdown-button]")
-//     if (!isDropdownButton && e.target.closest('[data-dropdown-container]') != null) return
-
-//     let currentDropdown
-//     if (isDropdownButton) {
-//         currentDropdown = document.querySelector('[data-dropdown]')
-//         currentDropdown.classList.toggle('active')
-//     }
-
-//     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-//         if (dropdown === currentDropdown) return
-//         dropdown.classList.remove('active')
-//     })
-
-// })
-
-// function toggle() {
-//     var blur = document.querySelector('[data-blur]')
-//     var menu = document.querySelector('[data-dropdown]')
-//     menu.classList.toggle('active')
-//     blur.classList.toggle('active')
-//     console.log('click')
-// }
-
 document.addEventListener('click', e => {
 
-    var blur = document.querySelector('[data-blur]')
-    var menu = document.querySelector('[data-dropdown]')
-    var menuBtn = e.target.matches('[data-dropdown-button]')
-    var menuCloseBtn = e.target.matches('[data-close-button]')
+    const blur = document.querySelector('[data-blur]')
+    const menu = document.querySelector('[data-dropdown]')
+    const menuBtn = e.target.matches('[data-dropdown-button]')
+    const menuCloseBtn = e.target.matches('[data-close-button]')
 
     // open dropdown menu (click menu button)
     if (menuBtn) {
